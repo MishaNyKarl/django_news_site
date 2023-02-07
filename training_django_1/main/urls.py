36619c1c196638254d1255dd5a8f3afd9d0ff0e9
MishaNyKarl <karlyavinmisha@gmail.com>
2023-02-07 23:41:08 +0300

    Sending django project on GitHub repository

---
+++ training_django_1/main/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about', views.about, name='about')
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
---
+++ training_django_1/news/url.py
@@ -0,0 +1,13 @@
+from . import views
+from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.news_home, name='news_home'),
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail'),
+    path('<int:pk>/update', views.NewsUpdateView.as_view(), name='news-update'),
+    path('<int:pk>/delete', views.NewsDeleteView.as_view(), name='news-delete'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
