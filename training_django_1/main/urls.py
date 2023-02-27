11e6ab2a2ae3b2c61fc44f09cfa4b970d353be93
Michael Karlyavin <karlyavinmisha@gmail.com>
2023-02-27 20:52:43 +0300

    Добавлена новая страница "Контакты"

--- training_django_1/main/urls.py
+++ training_django_1/main/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('contacts', views.contacts, name='contacts')
 ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
